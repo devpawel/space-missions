e4ddda1bb01be6ce24f77c8ecce97e0f70ff182a
devpawel <pawelkalinskidev@gmail.com>
2019-02-02 18:06:11 +0100

    Added description to card Added react-router

--- src/shared/components/SpaceCard.jsx
+++ src/shared/components/SpaceCard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Typography from '@material-ui/core/Typography';
 import SpaceshipImage from '../../shared/components/SpaceshipImage';
 import Countdown from 'react-countdown-now';
+import './SpaceCard.css';
 
 const style = {
   root: {
@@ -28,6 +29,20 @@ const style = {
 };
 
 export default class SpaceCard extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showDetails: false
+    };
+  }
+
+  showDetails(mouseOn) {
+    this.setState({
+      showDetails: mouseOn
+    });
+  }
+
   render() {
     // array of image sizes
     // "imageSizes": [320, 480, 640, 720, 768, 800, 960, 1024, 1080, 1280, 1440, 1920]
@@ -35,19 +50,56 @@ export default class SpaceCard extends Component {
     const launch = this.props.launch;
 
     return (
-      <div style={style.root}>
+      <div
+        style={style.root}
+        onMouseEnter={() => this.showDetails(true)}
+        onMouseLeave={() => this.showDetails(false)}
+      >
         <SpaceshipImage
           src={launch.rocket.imageURL}
           size={launch.rocket.imageSizes[imageSize]}
           alt={launch.rocket.name}
         />
-        <Typography style={style.title} variant="body2">
-          {launch.name}
-        </Typography>
         <Typography style={style.countdown} variant="body2">
           <Countdown date={launch.windowstart} />
         </Typography>
+        {this.state.showDetails ? (
+          <Typography style={style.title} variant="body2">
+            {launch.missions[0].description}
+          </Typography>
+        ) : (
+          <Typography style={style.title} variant="body2">
+            {launch.name}
+          </Typography>
+        )}
       </div>
     );
   }
 }
+
+// return (
+//   <div style={style.root} className="root">
+//     <div className="card">
+//       <div className="card-face card-front">
+//           <SpaceshipImage
+//             src={launch.rocket.imageURL}
+//             size={launch.rocket.imageSizes[imageSize]}
+//             alt={launch.rocket.name}
+//           />
+//           <Typography style={style.title} variant="body2">
+//             {launch.name}
+//           </Typography>
+//           <Typography style={style.countdown} variant="body2">
+//             <Countdown date={launch.windowstart} />
+//           </Typography>
+//       </div>
+//       <div className="card-face card-back">
+//         <SpaceshipImage
+//           src={launch.rocket.imageURL}
+//           size={launch.rocket.imageSizes[imageSize]}
+//           alt={launch.rocket.name}
+//         />
+//       </div>
+//     </div>
+//   </div>
+// );
