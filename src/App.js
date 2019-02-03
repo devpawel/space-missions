4d234a4a10f9da633658cf96873b90cd4969a87c
devpawel <pawelkalinskidev@gmail.com>
2019-02-03 19:43:32 +0100

    Added body background image

--- src/App.js
+++ src/App.js
@@ -1,8 +1,22 @@
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
 import LaunchList from './launch-list/containers/LaunchList';
 import LaunchDetail from './launch-detail/containers/LaunchDetail';
+import background from './assets/images/hubble_photo_2.jpg';
+
+const styles = theme => ({
+  '@global': {
+    body: {
+      backgroundImage: "url('" + background + "')",
+      backgroundRepeat: 'no-repeat',
+      backgroundPosition: 'center center',
+      backgroundSize: 'cover',
+      backgroundAttachment: 'fixed',
+      height: '100%'
+    }
+  }
+});
 
 class App extends Component {
   render() {
@@ -18,4 +32,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default withStyles(styles)(App);
