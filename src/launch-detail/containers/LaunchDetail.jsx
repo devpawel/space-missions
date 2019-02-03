b4a83448ad7ea40277f78626116b730abe14c440
devpawel <pawelkalinskidev@gmail.com>
2019-02-03 15:29:08 +0100

    Added react-json-view

--- src/launch-detail/containers/LaunchDetail.jsx
+++ src/launch-detail/containers/LaunchDetail.jsx
@@ -1,13 +1,41 @@
 import React, { Component } from 'react';
+import config from '../../config/api';
 import Typography from '@material-ui/core/Typography';
 import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import ReactJson from 'react-json-view';
 
 export default class LaunchDetail extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      launch: null
+    };
+  }
+
+  componentDidMount() {
+    const url = `${config.baseUrl}/launches/${this.props.match.params.id}`;
+    fetch(url)
+      .then(res => res.json())
+      .then(res =>
+        this.setState({
+          launch: res
+        })
+      )
+      .catch(err => console.log('Error:', err));
+  }
+
   render() {
     return (
       <Grid container justify="center">
         <Grid item>
-          <Typography variant="body2">{this.props.match.params.id}</Typography>
+          <Typography variant="body2">
+            <Paper>Hello</Paper>
+          </Typography>
+        </Grid>
+        <Grid item>
+          <ReactJson src={this.state.launch} theme="monokai" />
         </Grid>
       </Grid>
     );
