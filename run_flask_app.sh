9b2b2a4c9857331d9a64a6566fda2547c1769820
devpawel <pawelkalinskidev@gmail.com>
2019-02-24 14:20:56 +0100

    Fixed run flask script

--- run_flask_app.sh
+++ run_flask_app.sh
@@ -4,15 +4,6 @@ cd ./client
 yarn install
 yarn build
 
-echo Prepering virtual environment and installing flask dependencies for python
-cd ../server
-
-python -m venv venv
-source ./venv/Scripts/activate
-pip install -r requirements.txt
-
-echo "export FLASK_APP=app.py" >> ./venv/Scripts/activate
-
 echo Copy react build to flask
 cd ..
 
@@ -21,5 +12,13 @@ echo static copied
 
 cp -v ./client/build/*.{js,json,html} ./server/templates
 echo template copied
+
+echo Prepering virtual environment and installing flask dependencies for python
+cd ./server
+
+python -m venv venv
+echo "export FLASK_APP=app.py" >> ./venv/Scripts/activate
+source ./venv/Scripts/activate
+pip install -r requirements.txt
 
 flask run
